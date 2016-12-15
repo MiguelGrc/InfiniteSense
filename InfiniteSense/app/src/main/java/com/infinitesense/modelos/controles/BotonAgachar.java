2f3be563092c84def8a42d3bc380b881dae80489
xznexy <miguelparo12@hotmail.com>
2016-12-15 17:28:59 +0100

    Rollback...

--- InfiniteSense/app/src/main/java/com/infinitesense/modelos/controles/BotonAgachar.java
+++ InfiniteSense/app/src/main/java/com/infinitesense/modelos/controles/BotonAgachar.java
@@ -8,26 +8,30 @@ import com.infinitesense.gestores.CargadorGraficos;
 import com.infinitesense.modelos.Modelo;
 
 /**
- * Created by Jorge on 03/12/2016.
+ * Created by juanfa on 5/10/16.
  */
+
 public class BotonAgachar extends Modelo {
+
     public BotonAgachar(Context context) {
-        super(context, GameView.pantallaAncho*0.15 , GameView.pantallaAlto*0.5,
-            70,70);
+        super(context, GameView.pantallaAncho*0.15 , GameView.pantallaAlto*0.8 ,
+                GameView.pantallaAlto, GameView.pantallaAncho);
 
-    altura = (int) (GameView.pantallaAlto*0.60);
-    ancho = (int) (GameView.pantallaAlto*0.15);
-
-    imagen = CargadorGraficos.cargarDrawable(context, R.drawable.buttonfire);
-}
+        altura = 100;
+        ancho = 100;
+        imagen = CargadorGraficos.cargarDrawable(context, R.drawable.pad);
+    }
 
     public boolean estaPulsado(float clickX, float clickY) {
         boolean estaPulsado = false;
 
         if (clickX <= (x + ancho / 2) && clickX >= (x - ancho / 2)
-                && clickY <= (y + altura / 2) && clickY >= (y - altura / 2)) {
+                && clickY <= (y + altura / 2) && clickY >= (y - altura / 2)
+
+                ) {
             estaPulsado = true;
         }
         return estaPulsado;
     }
+
 }
